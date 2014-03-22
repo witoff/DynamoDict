8033f5f73f80aa0f4939842d4aa5b6d5494a8cfc
Robert J Witoff <robert.witoff@gmail.com>
2014-03-22 13:25:13 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
     setup(
         name = 'dynamodict',
         packages = ['dynamodict', 'dynamodict.ddbd'], # this must be the same as the name above
-        version = '0.2',
+        version = '0.2.1',
         description = 'A simple ORM that maps DynamoDB Tables to native python dicts',
         author = 'Rob Witoff',
         install_requires = ['boto>=2.25'],
         author_email = 'dynamodict@pspct.com',
         url = 'https://github.com/witoff/DynamoDict',   # use the URL to the github repo
-        download_url = 'https://github.com/witoff/DynamoDict/tarball/0.1', # I'll explain this in a second
+        download_url = 'https://github.com/witoff/DynamoDict/tarball/0.2.1', # I'll explain this in a second
         keywords = ['dynamodb', 'orm', 'python'], # arbitrary keywords
         classifiers = [],
     )
